43838d452817f745b4c54bfdbae6d419818c0618
Tomagochito <tbolanos@espol.edu.ec>
2024-07-29 10:57:30 -0500

    guia20

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/template_login', template_loginRouter);
+// no se usa app.use('/template_login', template_loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
